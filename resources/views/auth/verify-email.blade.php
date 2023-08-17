7f0d94c2025eada73c86ffd3eb041fdddb8acd1e
TebyTeb <esteban.c.ojedahdez@gmail.com>
2023-08-17 08:58:31 +0100

    Completa la leccion 187

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -1,30 +1,34 @@
 <x-guest-layout>
     <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
-        {{ __('Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another.') }}
+        {{ __('¡Gracias por registrarte! Antes de empezar deberías verificar tu email haciendo click en el enlace que acabamos de enviarte. Si no has recibido el email, haz click en el botón de "reenviar email de verificación" y te enviaremos uno nuevo.') }}
     </div>
 
     @if (session('status') == 'verification-link-sent')
-        <div class="mb-4 font-medium text-sm text-green-600 dark:text-green-400">
-            {{ __('A new verification link has been sent to the email address you provided during registration.') }}
+        <div class="mb-4 text-sm font-medium text-green-600 dark:text-green-400">
+            {{ __('Se ha enviado un nuevo link de verificación a la dirección de email que especificaste durante el registro.') }}
         </div>
     @endif
 
     <div class="mt-4 flex items-center justify-between">
-        <form method="POST" action="{{ route('verification.send') }}">
+        <form method="POST"
+            action="{{ route('verification.send') }}">
             @csrf
 
             <div>
                 <x-primary-button>
-                    {{ __('Resend Verification Email') }}
+                    {{ __('reenviar email de verificación') }}
                 </x-primary-button>
             </div>
         </form>
 
-        <form method="POST" action="{{ route('logout') }}">
+        <form method="POST"
+            action="{{ route('logout') }}">
             @csrf
 
-            <button type="submit" class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800">
-                {{ __('Log Out') }}
+            <button
+                class="rounded-md text-sm text-gray-600 hover:text-indigo-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:text-gray-400 dark:hover:text-indigo-100 dark:focus:ring-offset-gray-800"
+                type="submit">
+                {{ __('cerrar sesión') }}
             </button>
         </form>
     </div>
